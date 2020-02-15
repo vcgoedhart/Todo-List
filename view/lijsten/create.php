f449549bdef7aa59664694f97bb0c6baf1a5295c
Vincent <vcgoedhart@hotmail.com>
2020-02-15 02:41:13 +0100

    Commit

--- view/lijsten/create.php
+++ view/lijsten/create.php
@@ -1,12 +1,23 @@
 <?php include("../../_headerLayout.php"); ?>
 
+<?php
+if (isset($_POST["submit"])) {
+    $name = $_POST["inputName"];
+
+    $stmt = $conn->prepare("INSERT INTO lijsten (naam) VALUES (:name);");
+    $stmt->bindParam(":name", $name);
+    $stmt->execute();
+}
+?>
+
 <main class="container">
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <div class="form-group">
             <label for="inputName">Name</label>
-            <input type="text" class="form-control" id="inputName" placeholder="Name" required>
+            <input type="text" name="inputName" class="form-control" id="inputName" placeholder="Name" required>
         </div>
-        <button type="submit" class="btn btn-primary">Add</button>
+        <button type="submit" name="submit" class="btn btn-primary">Add</button>
     </form>
 </main>
+
 <?php include("../../_footerLayout.php"); ?>
