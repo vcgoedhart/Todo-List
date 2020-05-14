7d088da39e751873b3a87c79fd29448e292770ac
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-05-15 01:30:37 +0200

    Added filter options

--- view/lists/index.php
+++ view/lists/index.php
@@ -13,8 +13,13 @@ include("../../_layout/_headerLayout.php");
             <tr>
                 <th>ID</th>
                 <th>Description</th>
-                <th>Duration</th>
-                <th>Status</th>
+                <th>Duration
+                    <button class="btn btn-primary" onclick="filterDuration();">Switch</button>
+                </th>
+                <th>Status
+                    <button class="btn btn-primary" onclick="filterStatus();">Toggle</button>
+                    <button class="btn btn-primary" onclick="filterDefault();">Default</button>
+                </th>
                 <th>
                     <p>Options</p>
                 </th>
@@ -30,5 +35,55 @@ include("../../_layout/_headerLayout.php");
     var dbColumnName = "tasks";
 </script>
 <script src="../../script/tableLoader.js"></script>
+<script>
+    var durationToggle = 0,
+        statusToggle = "Incomplete";
+
+    /**
+     * Filters the table to show the duration from 'high - low' or 'low - high' 
+     */
+    function filterDuration() {
+        document.getElementById("table-container").innerHTML = "";
+
+        var toggleValue = durationToggle === 1 ? durationToggle = 0 : durationToggle = 1;
+        var arrayCopy = JSONArray;
+
+        if (toggleValue === 1) {
+            arrayCopy.sort(function(a, b) {
+                return parseFloat(a.duration) - parseFloat(b.duration);
+            });
+        } else {
+            arrayCopy.sort(function(a, b) {
+                return parseFloat(b.duration) - parseFloat(a.duration);
+            });
+        }
+
+        loadTable(1, arrayCopy);
+    }
+
+    /**
+     * Filters the table to only show the columns that are 'Complete' or 'Incomplete 
+     */
+    function filterStatus() {
+        document.getElementById("table-container").innerHTML = "";
+
+        var toggleValue = statusToggle === "Complete" ? statusToggle = "Incomplete" : statusToggle = "Complete";
+        var arrayCopy = JSONArray;
+
+        arrayCopy = arrayCopy.filter(function(obj) {
+            return obj.status !== toggleValue;
+        });
+
+        loadTable(1, arrayCopy);
+    }
+
+    /**
+     * Sets the table back to default
+     */
+    function filterDefault() {
+        document.getElementById("table-container").innerHTML = "";
+        loadTable(1);
+    }
+</script>
 
 <?php include("../../_layout/_footerLayout.php"); ?>
