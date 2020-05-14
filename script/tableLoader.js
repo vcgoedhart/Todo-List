3be8c92d8c6832cb71f2a24707ed6e7e83de27e0
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-05-14 02:40:47 +0200

    TableLoader is back

--- script/tableLoader.js
+++ script/tableLoader.js
@@ -1,36 +1,33 @@
-var container = document.getElementById("table-container");
-
 /** 
- * Creates a HTML element for every list item in the database.
- * "index = 1" is used to assign the position for every table item.
+ * Loads the items from the database into HTML
+ * 
+ * @param int index - Used to showcase the position of the item.
+ * @param string filterOption - Used to filter an item off the list.
  */
-var index = 1;
-for (const arr of JSONArray) {
-    var tr = document.createElement("tr");
+(function loadTable(index, filterOption, createNavigation, editNavigation) {
+    var container = document.getElementById("table-container");
 
-    // To go to its child page.
-    if (UrlExists("view/lists/index.php?list_id=" + arr['list_id'])) {
-        tr.onclick = function () {
-            window.location.href = "view/lists/index.php?list_id=" + arr['list_id'];
-        };
-    }
+    for (const arr of JSONArray) {
+        var tr = document.createElement("tr");
+        tr.setAttribute("data-id", arr['id']);
 
-    for (const item in arr) {
-        var td = document.createElement("td");
-        var textNode = document.createTextNode(arr[item]);
+        for (const item in arr) {
+            var td = document.createElement("td");
+            var textNode = document.createTextNode(arr[item]);
 
-        if (item.includes("id")) {
-            textNode = document.createTextNode(index);
-            index++;
+            if (item.includes("id")) {
+                textNode = document.createTextNode(index);
+                index++;
+            }
+
+            td.appendChild(textNode);
+            tr.appendChild(td);
         }
 
-        td.appendChild(textNode);
-        tr.appendChild(td);
+        createOptions(tr, arr['id']);
+        container.appendChild(tr);
     }
-
-    createOptions(tr, arr['list_id']);
-    container.appendChild(tr);
-}
+})(1);
 
 /**
  * Creates the edit & delete option for the list.
@@ -38,29 +35,17 @@ for (const arr of JSONArray) {
  * @param HTMLelement row - The <tr /> HTML row container element.
  * @param int list_id - The list id to provide more information for further action.
  */
-function createOptions(row, list_id) {
+function createOptions(row, id) {
     var td = document.createElement("td");
-    for (const option of ["edit", "delete"]) {
+    for (const option of ["delete", "edit"]) {
         var a = document.createElement("a");
         var textNode = document.createTextNode(option);
 
-        a.href = "view/lists/" + option + ".php?list_id=" + list_id;
+        a.href = "/School/Todo-list/view/" + dbColumnName + "/" + option + ".php?id=" + id;
 
         a.appendChild(textNode);
         td.appendChild(a);
     }
 
     row.appendChild(td);
-}
-
-/**
- * Checks if destination url exists. To prevent any complications.
- * 
- * @param string url - url to check 
- */
-function UrlExists(url) {
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return http.status != 404;
 }
