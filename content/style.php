8411a36e28841c6a0114246b0006f24099c8e952
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-05-15 02:28:31 +0200

    Minor changes

---
+++ content/style.php
@@ -0,0 +1,19 @@
+<style>
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  th>p,
+  td>a {
+    margin: 0 4px;
+    float: right;
+  }
+</style>
